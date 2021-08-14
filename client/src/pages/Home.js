64ff60e485d36888ffae4bcce8d1fb80ff15121b
Sebastian F <sebpfave2@gmail.com>
2021-08-14 16:20:25 -0400

    Implement useEffect to fetch all user thoughts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,26 @@ const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [thoughts, setThoughts] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('/api/users');
+        const jsonData = await res.json();
+
+        // sort array by createdAt property in descending order
+        const data = jsonData.sort((a, b) =>
+          a.createdAt < b.createdAt ? 1 : -1
+        );
+        setThoughts([...data]);
+        setIsLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <main>
       <div className="flex-row justify-space-between">
